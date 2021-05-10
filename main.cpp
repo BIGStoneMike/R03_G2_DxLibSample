460012145afa7f17f036939ac5f15b0c3d4dec27
BIGStoneMike <hj-g20004@sist.ac.jp>
2021-05-10 12:29:31 +0900

    キーボードの処理のソースファイル・ヘッダファイルをつくったああぁぁぁ。

--- main.cpp
+++ main.cpp
@@ -36,115 +36,21 @@ int WINAPI WinMain(
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	////四角の位置を決める
-	//int X = 0;
-	//int Y = 0;
-
-	//円の位置を決める
-	int X = GAME_WIDTH / 2;		//横の中心
-	int Y = GAME_HEIGHT / 2;	//縦の中心
-
-	////四角の大きさを決める
-	//int width = 32;		//幅
-	//int height = 32;	//高さ
-
-	//円の半径を決める
-	int radius = 100;	
-
-	//速度
-	int Speed = 1;
-	int XSpeed = 1;
-	int YSpeed = 1;
-
 	//ダブルバッファリング有効化
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	//無限ループ
 	while (1)
 	{
-		//何かしらのキーが押されたとき
-		if (CheckHitKeyAll() != 0)
-		{
-			break;	//無限ループを抜ける
-		}
-
 		//メッセージを受け取り続ける
-		if (ProcessMessage() != 0)	//-1のとき、エラーやウィンドウが閉じられた
-		{
-			break;	//無限ループを抜ける
-		}
-
-		if (ClearDrawScreen() != 0) { break; }
-
-		////四角を描画
-		//DrawBox(
-		//	X, Y, X + width, Y + height,
-		//	GetColor(255, 0, 0),	//色を取得
-		//	TRUE					//塗りつぶし
-		//);
-
-		//円を描画
-		DrawCircle(
-			X, Y, radius,
-			GetColor(0, 255, 0),	//色を取得
-			FALSE, 5
-		);
-
-		X += XSpeed;					//四角の位置を右にずらす
-
-		//質問①　四角を斜め右下に動かそう
-		Y += YSpeed;
-
-		////質問②　四角を画面の端にきたら、移動する向きを反転させてみよう！
-		//if (X < 0 || X + width > GAME_WIDTH)		//画面の横から出たとき
-		//{
-		//	//移動速度の符号を反転させる
-		//	//+1　なら、-1　する　/　-1　ならば、+1　にする
-		//	XSpeed = -XSpeed;					//移動する向きを反転
-
-		//	//壁に当たったら速くなる
-		//	if (XSpeed > 0) { XSpeed += 10; }
-		//	else if (XSpeed < 0) { XSpeed -= 10; }
-		//}
-
-		//if (Y < 0 || Y + height > GAME_HEIGHT)		//画面の縦から出たとき
-		//{
-		//	//移動速度の符号を反転させる
-		//	//+1　なら、-1　する　/　-1　ならば、+1　にする
-		//	YSpeed = -YSpeed;					//移動する向きを反転
-
-		//	//壁に当たったら速くなる
-		//	if (YSpeed > 0) { YSpeed += 10; }
-		//	else if (YSpeed < 0) { YSpeed -= 10; }
-		//}
-
-		//円が画面の端にきたら、移動する向きを変える
-		if (X - radius < 0 || X + radius > GAME_WIDTH)		//画面の横から出たとき
-		{
-			//移動速度の符号を反転させる
-			//+1　なら、-1　する　/　-1　ならば、+1　にする
-			XSpeed = -XSpeed;					//移動する向きを反転
-
-			//壁に当たったら速くなる
-			if (XSpeed > 0) { XSpeed += 10; }
-			else if (XSpeed < 0) { XSpeed -= 10; }
-		}
-
-		if (Y - radius< 0 || Y + radius > GAME_HEIGHT)		//画面の縦から出たとき
-		{
-			//移動速度の符号を反転させる
-			//+1　なら、-1　する　/　-1　ならば、+1　にする
-			YSpeed = -YSpeed;					//移動する向きを反転
-
-			//壁に当たったら速くなる
-			if (YSpeed > 0) { YSpeed += 10; }
-			else if (YSpeed < 0) { YSpeed -= 10; }
-		}
+		if (ProcessMessage() != 0) { break; }	//-1のとき、エラーやウィンドウが閉じられた	
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去する
 
 		ScreenFlip();				//ダブルバッファリングした画面を描画
 	}
-
-		DxLib_End();			// ＤＸライブラリ使用の終了処理
+		
+		// ＤＸライブラリ使用の終了処理
+		DxLib_End();
 
 		return 0;				// ソフトの終了 
 }

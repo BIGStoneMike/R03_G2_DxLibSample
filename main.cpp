7e8acb32d10741956e60037aebbf110db10049df
BIGStoneMike <hj-g20004@sist.ac.jp>
2021-06-08 12:27:59 +0900

    効果音を追加しました。

--- main.cpp
+++ main.cpp
@@ -66,6 +66,9 @@ AUDIO TitleBGM;
 AUDIO PlayBGM;
 AUDIO EndBGM;
 
+//効果音
+AUDIO PlayerSE;
+
 //画面の切り替え
 BOOL IsFadeOut = FALSE;		//フェードアウト
 BOOL IsFadeIn = FALSE;		//フェードイン
@@ -227,6 +230,8 @@ int WINAPI WinMain(
 	DeleteSoundMem(PlayBGM.handle);			//音楽をメモリ上から削除
 	DeleteSoundMem(EndBGM.handle);			//音楽をメモリ上から削除
 
+	DeleteSoundMem(PlayerSE.handle);			//音楽をメモリ上から削除
+
 	//ＤＸライブラリ使用の終了処理
 	DxLib_End();
 
@@ -315,7 +320,10 @@ BOOL GameLoad(VOID)
 	if (!LoadAudio(&TitleBGM, ".\\Audio\\natsunokiri.mp3", 255, DX_PLAYTYPE_LOOP)) { return FALSE; }
 	if (!LoadAudio(&PlayBGM, ".\\Audio\\uzumaki.mp3", 255, DX_PLAYTYPE_LOOP)) { return FALSE; }
 	if (!LoadAudio(&EndBGM, ".\\Audio\\asaitsukiakari.mp3", 255, DX_PLAYTYPE_LOOP)) { return FALSE; }
+
+	if (!LoadAudio(&PlayerSE, ".\\Audio\\驚く.mp3", 255, DX_PLAYTYPE_BACK)) { return FALSE; }
 	
+
 	return TRUE;	//全て読み込みた！
 }
 
@@ -476,19 +484,47 @@ VOID PlayProc(VOID)
 	if (KeyDown(KEY_INPUT_UP) == TRUE)
 	{
 		player.y -= player.speed * fps.DeltaTime;
+
+		//効果音が流れていないとき
+		if (CheckSoundMem(PlayerSE.handle) == 0)
+		{
+			//効果音を流す
+			PlaySoundMem(PlayerSE.handle, PlayerSE.playType);
+		}
 	}
 	if (KeyDown(KEY_INPUT_DOWN) == TRUE)
 	{
 		player.y += player.speed * fps.DeltaTime;
+		
+		//効果音が流れていないとき
+		if (CheckSoundMem(PlayerSE.handle) == 0)
+		{
+			//効果音を流す
+			PlaySoundMem(PlayerSE.handle, PlayerSE.playType);
+		}
 	}
 
 	if (KeyDown(KEY_INPUT_LEFT) == TRUE)
 	{
 		player.x -= player.speed * fps.DeltaTime;
+
+		//効果音が流れていないとき
+		if (CheckSoundMem(PlayerSE.handle) == 0)
+		{
+			//効果音を流す
+			PlaySoundMem(PlayerSE.handle, PlayerSE.playType);
+		}
 	}
 	if (KeyDown(KEY_INPUT_RIGHT) == TRUE)
 	{
 		player.x += player.speed * fps.DeltaTime;
+
+		//効果音が流れていないとき
+		if (CheckSoundMem(PlayerSE.handle) == 0)
+		{
+			//効果音を流す
+			PlaySoundMem(PlayerSE.handle, PlayerSE.playType);
+		}
 	}
 
 	//当たり判定を更新する
